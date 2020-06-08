c6cce7032cee889306541c1565a44ef63a9ac45c
ZReKoJ <zhong.9745@gmail.com>
2020-06-09 00:55:48 +0200

    added two modes, drawing and by file

--- js/index.js
+++ js/index.js
@@ -2,6 +2,17 @@
 
 var notifier = new Notifier();
 var map;
+var raphael;
+var raphaelParams = {
+    x: {
+        max: 0,
+        min: Number.MAX_VALUE
+    },
+    y: {
+        max: 0,
+        min: Number.MAX_VALUE
+    }
+};
 var data = [];
 
 $(() => {
@@ -10,10 +21,101 @@ $(() => {
     makeResizableDiv('.setting-panel');
 
     map = $(".draw-panel");
+    worldMap();
+
+    let settingFunctions = settingPanel('.setting-panel>.setting');
+});
+
+function loadDataFile(link, callback) {
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = () => {
+        if (request.readyState == 4) {
+            callback(parseData(request.responseText));
+        }
+    };
+    request.open("GET", link, true);
+    request.send();
+}
+
+// This parse is very simple, so it could get error in case the format is not correct
+// All point variables should be starting with "X XXX XXX" as index, number, number
+// the other values will be filtered 
+function parseData(data) {
+    raphaelParams.x.max = 0;
+    raphaelParams.x.min = Number.MAX_VALUE;
+    raphaelParams.y.max = 0;
+    raphaelParams.y.min = Number.MAX_VALUE;
+    let parsedData = data.split("\n").map(
+        lines => lines.split(" ")
+    ).filter(
+        lines => Number(lines[0]) != "" && Number.isInteger(Number(lines[0]))
+    ).map(lines => {
+        raphaelParams.x.max = Math.max(lines[2], raphaelParams.x.max);
+        raphaelParams.x.min = Math.min(lines[2], raphaelParams.x.min);
+        raphaelParams.y.max = Math.max(lines[1], raphaelParams.y.max);
+        raphaelParams.y.min = Math.min(lines[1], raphaelParams.y.min);
+        return [Number(lines[1]), Number(lines[2])]
+    });
+    return parsedData.sort(() => 0.5 - Math.random()).slice(0, 1000);
+    return parsedData.sort(() => 0.5 - Math.random()).slice(0, 1000).map(values => [
+        mapSize.height - ((values[0] - y.min) * mapSize.height / (y.max - y.min)),
+        (values[1] - x.min) * mapSize.width / (x.max - x.min)
+    ]);
+}
+
+function settingPanel(div) {
+    let setting = $(div);
+
+    let executeButton = setting.find("button.execute");
+    executeButton.on("click", () => {
+        console.log(data);
+    });
+
+    let clearButton = setting.find("button.clear");
+    clearButton.on("click", () => {
+        map.trigger('update', {
+            deletePlotKeys: "all",
+            deleteLinkKeys: "all",
+            animDuration: 1000
+        })
+    });
+
+    let methodSelect = setting.find(".methods");
+    methodSelect.on("change", (e) => {
+        $(".draw-panel").empty();
+        if (methodSelect.val() == "world") {
+            worldMap();
+        } else {
+            filesMap(methodSelect.val());
+        }
+    });
+}
+
+function filesMap(country) {
+    raphael = new Raphael("draw-panel", map.width(), map.height());
+    loadDataFile("https://zrekoj.github.io/Travelling-Salesman-Problem/data/" + country + ".txt", (parsedData) => {
+        data = parsedData;
+        parsedData.forEach((dataValue) => {
+            drawCircle(dataValue[0], dataValue[1]);
+        });
+    });
+}
+
+function drawCircle(y, x) {
+    raphael.circle(
+        (x - raphaelParams.x.min) * map.width() / (raphaelParams.x.max - raphaelParams.x.min),
+        map.height() - ((y - raphaelParams.y.min) * map.height() / (raphaelParams.y.max - raphaelParams.y.min)),
+        1
+    ).attr("stroke", "#ffffff");
+}
+
+function worldMap() {
+    map.append('<div class="map">Cargando ...</div>');
+
     // Mapael initialisation
     map.mapael({
         map: {
-            name: "yemen",
+            name: "world_countries",
             zoom: {
                 enabled: true,
                 step: 0.25,
@@ -34,72 +136,8 @@ $(() => {
                 y: coords.y,
                 size: 5
             }
-            data.push({
-                x: coords.x,
-                y: coords.y,
-            });
+            data.push([coords.x, coords.y]);
             map.trigger('update', [updateOptions]);
         }
-    });
-
-    let settingFunctions = settingPanel('.setting-panel>.setting');
-});
-
-function loadDataFile(link, callback) {
-    let request = new XMLHttpRequest();
-    request.onreadystatechange = () => {
-        if (request.readyState == 4) {
-            callback(parseData(request.responseText));
-        }
-    };
-    request.open("GET", link, true);
-    request.send();
-}
-
-// This parse is very simple, so it could get error in case the format is not correct
-// All point variables should be starting with "X XXX XXX" as index, number, number
-// the other values will be filtered 
-function parseData(data) {
-    return data.split("\n").map(
-        lines => lines.split(" ")
-    ).filter(
-        lines => Number(lines[0]) != "" && Number.isInteger(Number(lines[0]))
-    ).map(
-        lines => [Number(lines[2]), Number(lines[1])]
-    );
-}
-
-function settingPanel(div) {
-    let setting = $(div);
-
-    let executeButton = setting.find("button.execute");
-    executeButton.on("click", () => {});
-
-    let clearButton = setting.find("button.clear");
-    clearButton.on("click", () => {
-        map.trigger('update', {
-            deletePlotKeys: "all",
-            deleteLinkKeys: "all",
-            animDuration: 1000
-        })
-    });
-
-    let methodSelect = setting.find(".methods");
-    methodSelect.on("change", (e) => {
-        loadDataFile("https://zrekoj.github.io/Travelling-Salesman-Problem/data/" + methodSelect.val() + ".txt", (parsedData) => {
-            let plots = {}
-            parsedData.forEach((data) => {
-                let updateOptions = {
-                    'newPlots': {}
-                };
-                let plotId = String(data[0]) + '-' + String(data[1]);
-                updateOptions.newPlots[plotId] = {
-                    x: data[0] / 100,
-                    y: data[1] / 100,
-                    size: 0.5
-                }
-                map.trigger('update', [updateOptions]);
-            })
-        })
     });
 }
